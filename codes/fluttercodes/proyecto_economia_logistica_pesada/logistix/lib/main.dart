03bc669a7cd850484d6dede619a14655d606bba9
RaulDev <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>
2023-06-25 21:47:54 -0400

    agregando la estructura de carpetas

---
+++ codes/fluttercodes/proyecto_economia_logistica_pesada/logistix/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const Logistix());
+
+class Logistix extends StatelessWidget {
+  const Logistix({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
