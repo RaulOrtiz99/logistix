ad5bad511ced2b6ddc9607832673d9f353dbfead
RaulDev <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>
2023-07-26 23:14:59 -0400

    ya por fin tenemos mapa visualizado

--- codes/fluttercodes/proyecto_economia_logistica_pesada/logistix/lib/screens/map_screen.dart
+++ codes/fluttercodes/proyecto_economia_logistica_pesada/logistix/lib/screens/map_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:logistix/blocs/location/location_bloc.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -31,9 +32,12 @@ class _MapScreenState extends State<MapScreen> {
     return Scaffold(body: BlocBuilder<LocationBloc, LocationState>(
       builder: (context, state) {
         if (state.lastKnownLocation == null)
+          // ignore: curly_braces_in_flow_control_structures
           return const Center(child: Text('Espere por favor...'));
-        return Center(
-          child: Text('${state.lastKnownLocation!.latitude}, ${state.lastKnownLocation!.longitude}'),
+        CameraPosition initialCameraPosition = CameraPosition(
+            target: state.lastKnownLocation!, zoom: 15);
+        return GoogleMap(
+          initialCameraPosition: initialCameraPosition,
         );
       },
     ));
